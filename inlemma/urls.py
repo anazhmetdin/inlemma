9b796a249def3b7453693c36dc25b721e1b46a1c
anazhmetdin <anazhmetdin@gmail.com>
2022-05-17 13:46:45 +0200

    add google authentication

--- inlemma/urls.py
+++ inlemma/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('social_django.urls', namespace='social')),
     path('', include('accounts.urls')),
 ]
