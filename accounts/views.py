02a7ba2a0d9e1a93e02402ce32a5e6c73c39d323
anazhmetdin <anazhmetdin@gmail.com>
2022-05-21 10:27:57 +0200

    restructure the templates

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.forms.utils import ErrorList
 from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.views import View
 
@@ -10,20 +11,28 @@ class loginView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('home')
-        return render(request, 'accounts/login.html', {})
+        context = {'form': AuthenticationForm()}
+        return render(request, 'accounts/login.html', context)
 
     def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(request.POST)
+        
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
             login(request, user)
             # Redirect to a success page.
             return redirect('home')
+        
+        # Return an 'invalid login' error message.
+        if form.errors:
+            for error in form.errors:
+                messages.error(request, form.errors[error])
         else:
-            # Return an 'invalid login' error message.
             messages.error(request, ErrorList(["Wrong username or password."]))
-            return redirect('login')
+        
+        return redirect('login')
 
 class logoutView(View):
     def get(self, request):
@@ -39,12 +48,14 @@ class registerView(View):
 
     def post(self, request):
         form = NewUserForm(request.POST)
+
         if form.is_valid():
             user = form.save()
             login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             messages.success(request, ErrorList(["Registration successful."]) )
             return redirect("home")
+        
         for error in form.errors:
             messages.error(request, form.errors[error])
-            print(type(form.errors[error]))
+        
         return redirect('register')
