620524ef6b0d54c8c6b2b9170fb2fd3860cd1b54
anazhmetdin <anazhmetdin@gmail.com>
2022-05-24 18:47:46 +0200

    add pagination and infinite scrolling to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import redirect, render
-from django.views import View
+from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from posts.models import post
 
-class profileView(View):
-    def get(self, request, username):
-        user_query = User.objects.filter(username__iexact=username)
+class profileView(ListView):
+    model= post
+    paginate_by= 10
+    template_name= 'profiles/profile.html'
+    context_object_name= 'posts'
+
+    def get_queryset(self):
+        user_query = User.objects.filter(username__iexact=self.kwargs['username'])
+        
         if user_query:
             user = user_query.get()
-            isOwner = user.username == request.user.username
-            # if logged-in user is the profile owner, display anonymous posts
-            try:
-                if isOwner:
-                    posts = post.objects.filter(user=user).all()
-                else:
-                    posts = post.objects.filter(user=user, anonymous=False, published=True).all()
-            except post.DoesNotExist:
-                posts = None
-            
-            context = {'user': user, 'posts': posts, 'isOwner': isOwner}
-            return render(request, 'profiles/profile.html', context)
-        else:
-            return redirect('home')
+
+        isOwner = user.username == self.request.user.username
+
+        self.profileUser = user
+        self.isOwner = isOwner
+        
+        try:
+            if isOwner:
+                posts = post.objects.filter(user=user).all()
+            else:
+                posts = post.objects.filter(user=user, anonymous=False, published=True).all()
+            posts = posts.order_by('-id')
+        except post.DoesNotExist:
+            posts = []
+        
+        return posts
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['isOwner'] = self.isOwner
+        context['profileUser'] = self.profileUser
+        return context
