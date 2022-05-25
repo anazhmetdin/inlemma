063365f37521134464461694a4d4e576291e5847
anazhmetdin <anazhmetdin@gmail.com>
2022-05-25 21:54:55 +0200

    ocd stuff

--- home/static/home/js/autoHideCK.js
+++ home/static/home/js/autoHideCK.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {  
     CKEDITOR.on('instanceReady', function (ev) {  
        document.getElementById(ev.editor.id + '_bottom').style.display = 'none';  
- 
+       document.getElementById('newPostBody').style.display='block' 
+       document.getElementById('dummyNewPost').style.display='none' 
  
        ev.editor.on('focus', function (e) {  
-          document.getElementById(ev.editor.id + '_bottom').style.display = 'block';  
- 
+          document.getElementById(ev.editor.id + '_bottom').style.display = 'block'; 
        });  
        ev.editor.on('blur', function (e) {  
           document.getElementById(ev.editor.id + '_bottom').style.display = 'none';  
--- home/static/main/js/resizableText.js
+++ home/static/main/js/resizableText.js
@@ -3,7 +3,8 @@ $(".resizable_textarea").each(
         this.setAttribute("style", "height:" + (this.scrollHeight) + "px;");
     }
 ).on("input", function () {
-        this.style.height = "auto";
-        this.style.height = (this.scrollHeight) + "px";
+        this.style.height = "3.5em";
+        // this.style.height = (this.scrollHeight) + "px";
+        if (this.scrollHeight >= 80) { this.style.height = 5 + "px"; this.style.height = (this.scrollHeight + 10) + "px"; }
     }
 );
