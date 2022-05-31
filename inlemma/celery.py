3000746cfc16338ebbe322bb0ed76d0d09d4147d
anazhmetdin <anazhmetdin@gmail.com>
2022-05-31 19:31:44 +0200

    create doc2vec model every day of all posts

--- inlemma/celery.py
+++ inlemma/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inlemma.settings')
@@ -12,6 +13,14 @@ app = Celery('inlemma')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Celery Beat Settings
+app.conf.beat_schedule = {
+    'update-D2Vmodel-every-night': {
+        'task': 'spaces.updateD2Vmodel',
+        'schedule': crontab(hour=0, minute=0)
+    },
+}
 
 # load task modules from all registered Django app configs.
 app.autodiscover_tasks()
