26a82474cbeaababff1eb06b1d1db11d4400ab11
anazhmetdin <anazhmetdin@gmail.com>
2022-05-31 23:06:04 +0200

    add ProcessedPost model and link it to everything

--- spaces/models.py
+++ spaces/models.py
@@ -1,3 +1,74 @@
+import string
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from posts.models import Post
+import bleach
+from html2text import html2text
+import threading
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+# download the latest version of nltk datasets
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# Create your models here.
+
+class ProcessedPost(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE, db_index=True)
+    body = models.TextField(blank=False, null=False)
+
+
+@receiver(models.signals.post_save, sender=Post)
+def post_saved(sender, instance, **kwargs):
+    processedPost = ProcessedPost.objects.create(post=instance,
+                                                 body=instance.body)
+    PostProcessor(processedPost).run()
+
+
+class PostProcessor(threading.Thread):
+    def __init__(self, processedPost):
+        self.processedPost = processedPost
+        threading.Thread.__init__(self)
+
+    def tokenize(self, doc, stop_words):
+        tokens = word_tokenize(doc.lower())
+        return " ".join([w for w in tokens if not w in stop_words])
+    
+    def process(self, body):
+        ONE_OR_MORE = "+"
+        START = "^"
+        END = "$"
+
+        PUNCTUATION = '[^\w\s]' #^=not, \w=alphanumeric character, \s=space
+        SPACE = "\s"
+        ERASE = ""
+        NEWLINES = "[\r\n\t]"
+
+        stop_words = set(stopwords.words('english'))
+        
+        processedBody = body.lower()
+        
+        processedBody = re.sub(PUNCTUATION, ' ', processedBody)
+        processedBody = processedBody.translate(str.maketrans(dict.fromkeys(string.punctuation)))
+        processedBody = re.sub(NEWLINES, ' ', processedBody)
+        processedBody = re.sub(SPACE+ONE_OR_MORE, " ", processedBody)
+        processedBody = re.sub(SPACE, " ", processedBody)
+        processedBody = re.sub(START+SPACE, ERASE, processedBody)
+        processedBody = re.sub(SPACE+END, ERASE, processedBody)
+        processedBody = self.tokenize(processedBody, stop_words)
+        
+        return processedBody
+
+    def run(self):
+        cleanedBody = bleach.clean(self.processedPost.body,
+                                   tags=settings.BLEACH_ALLOWED_STYLES,
+                                   attributes=settings.BLEACH_ALLOWED_ATTRIBUTES)
+        body = html2text(cleanedBody)
+        self.processedPost.body = self.process(body)
+        self.processedPost.save()
+        from .utils import inlemmaEngine
+        vector = inlemmaEngine.D2Vmodel.infer_vector(self.processedPost.body.split())
+        inlemmaEngine.D2Vmodel.dv.add_vector(str(self.processedPost.post.id), vector)
+        inlemmaEngine.D2Vmodel.dv.norms = None
--- spaces/utils.py
+++ spaces/utils.py
@@ -2,16 +2,14 @@ import multiprocessing as mp
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from datetime import datetime
 from posts.models import Post
-import logging
 from django.conf import settings
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+from .models import ProcessedPost
 
 
 class TaggedCorpus:        
     def __iter__(self):
-        for post in Post.objects.all():
-            title, words = post.id, post.body
+        for processedPost in ProcessedPost.objects.all():
+            title, words = str(processedPost.post.id), processedPost.body
             words = words.split()
             # if len(words) < 75:
             #     continue
@@ -19,7 +17,7 @@ class TaggedCorpus:
 
 
 class inlemmaCore():
-    def __init__(self, D2VmodelPath=f'{settings.BASE_DIR}/spaces/D2Vmodels/d2vmodel_latest.model'):
+    def __init__(self, D2VmodelPath=f'{settings.BASE_DIR}/spaces/D2Vmodels/latest.model'):
         try:
             self.D2Vmodel = self.loadD2Vmodel(D2VmodelPath)
         except:
@@ -31,7 +29,7 @@ class inlemmaCore():
     
     def updateD2Vmodel(self):
         try:
-            self.D2Vmodel.save(f'{settings.BASE_DIR}/spaces/D2Vmodels/d2vmodel_{datetime.now().strftime("%d_%m_%Y_%H_%M")}.model')
+            self.D2Vmodel.save(f'{settings.BASE_DIR}/spaces/D2Vmodels/{datetime.now().strftime("%d_%m_%Y_%H_%M")}.model')
         except:
             pass
         documents = TaggedCorpus()
@@ -45,18 +43,23 @@ class inlemmaCore():
         model_dm.build_vocab(documents, progress_per=100000)
         model_dm.train(documents, total_examples=model_dm.corpus_count, epochs=model_dm.epochs, report_delay=15*60)
         self.D2Vmodel = model_dm
-        self.D2Vmodel.save(f'{settings.BASE_DIR}/spaces/D2Vmodels/d2vmodel_latest.model')
+        self.D2Vmodel.save(f'{settings.BASE_DIR}/spaces/D2Vmodels/latest.model')
 
     def similarPosts(self, post, n=5):
         similars = self.__similarPosts(post, n)
-        return [Post.objects.get(id=similiar[0]) for similiar in similars]
+        print(similars)
+        return [Post.objects.get(id=int(similiar[0])) for similiar in similars]
 
     def __similarPosts(self, post, n):
-        if post.id in self.D2Vmodel.dv:
-             return self.D2Vmodel.dv.most_similar(positive=[post.id], topn=n)
-        else:
-            vector = self.D2Vmodel.infer_vector(post.body.split())
-            return self.D2Vmodel.dv.most_similar(vector, topn=n)
+        if str(post.id) not in self.D2Vmodel.dv:
+            try:
+                processedPost = ProcessedPost.objects.get(post=post)
+            except:
+                return []
+            vector = self.D2Vmodel.infer_vector(processedPost.body.split())
+            self.D2Vmodel.dv.add_vector(str(post.id), vector)
+            self.D2Vmodel.dv.norms = None
+        return self.D2Vmodel.dv.most_similar(positive=[str(post.id)], topn=n)
 
 
 inlemmaEngine = inlemmaCore()
