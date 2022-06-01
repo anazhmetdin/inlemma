8ea57abe1d35f6a51601023e8cf8457076328e18
anazhmetdin <anazhmetdin@gmail.com>
2022-06-01 03:24:36 +0200

    adjust new comment section

--- home/static/main/js/resizableText.js
+++ home/static/main/js/resizableText.js
@@ -3,7 +3,7 @@ $(".resizable_textarea").each(
         this.setAttribute("style", "height:" + (this.scrollHeight) + "px;");
     }
 ).on("input", function () {
-        this.style.height = "3.5em";
+        this.style.height = "80px";
         // this.style.height = (this.scrollHeight) + "px";
         if (this.scrollHeight >= 80) { this.style.height = 5 + "px"; this.style.height = (this.scrollHeight + 10) + "px"; }
     }
